a37f8f9ebb485071ec59da0a87005b64ab07f6f0
naran-panthi <nrn.panthi@gmail.com>
2019-09-06 15:24:42 +0545

    sep 3 commits view pager

---
+++ app/src/main/java/com/nepal/andoriddemo/helpers/CustomImageView.java
@@ -0,0 +1,42 @@
+package com.nepal.andoriddemo.helpers;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by NaRan on 23,Sep,2018.
+ * Copyright (c) UT Pvt. Ltd. All rights reserved.
+ * nrn.panthi@gmail.com
+ * MacBook
+ **/
+public class CustomImageView extends ImageView {
+
+    public static float radius = 10.0f;
+
+    public CustomImageView(Context context) {
+        super(context);
+    }
+
+    public CustomImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //float radius = 36.0f;
+        @SuppressLint("DrawAllocation") Path clipPath = new Path();
+        @SuppressLint("DrawAllocation") RectF rect = new RectF(0, 0, this.getWidth(), this.getHeight());
+        clipPath.addRoundRect(rect, radius, radius, Path.Direction.CW);
+        canvas.clipPath(clipPath);
+        super.onDraw(canvas);
+    }
+}
